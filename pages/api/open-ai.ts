c5697edc62bdfc2cca55b6f0e5b03e8a18f85fbd
Reinaldo Haynes <rey.haynes@me.com>
2023-02-02 22:28:07 -0500

    Format fix

--- pages/api/open-ai.ts
+++ pages/api/open-ai.ts
@@ -1,37 +1,42 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { Configuration, OpenAIApi } from "openai";
+import { NextApiRequest, NextApiResponse } from "next"
+import { Configuration, OpenAIApi } from "openai"
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-});
+})
 
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration)
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+const request = async (req: NextApiRequest, res: NextApiResponse) => {
   if (!configuration.apiKey) {
-    res.status(500).json({ error: "No API key set." });
+    res.status(500).json({ error: "No API key set." })
   }
 
-  const { prompt } = req.body;
+  const { prompt } = req.body
+
+  // Use this to fine tune the prompt without user input
+  // eg. Get response in Markdown or Ask to always explain code
+  const promptAdditions = ``
 
   if (!prompt || prompt.length === 0) {
-    res.status(400).json({ error: "No prompt provided." });
+    res.status(400).json({ error: "No prompt provided." })
   }
 
   try {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: prompt,
+      prompt: prompt + promptAdditions,
       temperature: 0.5,
       max_tokens: 3500,
     })
 
-    res.status(200).json({ 
+    res.status(200).json({
       result: response.data.choices[0].text,
-      full: response.data
-    });
+      full: response.data,
+    })
   } catch (err) {
-    res.status(500).json({ error: err });
+    res.status(500).json({ error: err })
   }
 }
+
+export default request
